255c43f76d5990167a02c4e8f4cce6b12feb07c8
William Sadler <WGSadler@gmail.com>
2021-01-03 20:51:40 +0000

    Done bruh

--- routes.py
+++ routes.py
@@ -19,19 +19,25 @@ def index():
 def initial_download():
    print("Downloading First Few Videos...")
 
-   urls = ["https://www.reddit.com/r/perfectlycutscreams/comments/cvt1s9/nword_ticket/",
-            "https://www.reddit.com/r/perfectlycutscreams/comments/hkyiz5/i_dont_think_he_has_a_gf_anymore/",
-            "https://www.reddit.com/r/perfectlycutscreams/comments/ehmr54/i_peaked_in_terms_of_laziness_last_night/"]
+   print(request.form['post1'])
+   print(request.form['post2'])
+   print(request.form['post3'])
+
+   urls = [request.form['post1'], request.form['post2'], request.form['post3']]
 
    for i in range(0, len(urls)):
       url = urls[i]
       download_reddit_video(url)
 
+@app.route('/reset_folders', methods = ['POST'])
+def reset_folder():
+   print("Resetting Folders...")
+   setup()
+
 @app.route('/download_video', methods = ['POST'])
 def download_video():
    print("Downloading Video...")
-
-   url = request.form['post_urls']
+   url = request.form['post_url']
 
    download_reddit_video(url)
 
@@ -50,6 +56,13 @@ def download_reddit_video(url):
 
    v = glob.glob(os.getcwd() + "\\static\\videos\\dl\\*.mp4")[0]
    os.rename(v, os.getcwd() + "/".join(v.split("/")[:-1]) + "\\static\\videos\\"+str(counter)+".mp4")
+
+   #remove old videos
+   try:
+      os.remove(os.getcwd() + "\\static\\videos\\"+str(counter-3)+".mp4")
+   except:
+      print("Failed trying to delete old video.")
+
    counter = counter + 1
 
 def setup():
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,25 +2,69 @@ console.log("Javascript Loaded.");
 currrent_video_id = 0
 firstRun = true
 
-clips_processed = ["https://www.reddit.com/r/perfectlycutscreams/comments/cvt1s9/nword_ticket/",
-"https://www.reddit.com/r/perfectlycutscreams/comments/hkyiz5/i_dont_think_he_has_a_gf_anymore/",
-"https://www.reddit.com/r/perfectlycutscreams/comments/ehmr54/i_peaked_in_terms_of_laziness_last_night/"]
+clips_processed = []
 
 next_video = "";
 
 document.getElementById("goodbutton").onclick = function(){
     document.getElementById("goodoutput").textContent += clips_processed[currrent_video_id] + "\n";
+
+    var bad_lines = document.getElementById("badoutput").textContent.split("\n")
+    var long_lines = document.getElementById("longoutput").textContent.split("\n")
+
+    if(bad_lines.includes(clips_processed[currrent_video_id])){
+        index = bad_lines.indexOf(clips_processed[currrent_video_id])
+        bad_lines.splice(index, 1)
+        document.getElementById("badoutput").textContent = bad_lines.join("\n")
+    }
+
+    if(long_lines.includes(clips_processed[currrent_video_id])){
+        index = long_lines.indexOf(clips_processed[currrent_video_id])
+        long_lines.splice(index, 1)
+        document.getElementById("longoutput").textContent = long_lines.join("\n")
+    }
 }
 
 document.getElementById("badbutton").onclick = function(){
     document.getElementById("badoutput").textContent += clips_processed[currrent_video_id]+ "\n";
+
+    var good_lines = document.getElementById("goodoutput").textContent.split("\n")
+    var long_lines = document.getElementById("longoutput").textContent.split("\n")
+
+    if(good_lines.includes(clips_processed[currrent_video_id])){
+        index = good_lines.indexOf(clips_processed[currrent_video_id])
+        good_lines.splice(index, 1)
+        document.getElementById("goodoutput").textContent = good_lines.join("\n")
+    }
+
+    if(long_lines.includes(clips_processed[currrent_video_id])){
+        index = long_lines.indexOf(clips_processed[currrent_video_id])
+        long_lines.splice(index, 1)
+        document.getElementById("longoutput").textContent = long_lines.join("\n")
+    }
 }
 
 document.getElementById("longbutton").onclick = function(){
     document.getElementById("longoutput").textContent += clips_processed[currrent_video_id] + "\n";
+
+    var good_lines = document.getElementById("goodoutput").textContent.split("\n")
+    var bad_lines = document.getElementById("badoutput").textContent.split("\n")
+
+    if(good_lines.includes(clips_processed[currrent_video_id])){
+        index = good_lines.indexOf(clips_processed[currrent_video_id])
+        good_lines.splice(index, 1)
+        document.getElementById("goodoutput").textContent = good_lines.join("\n")
+    }
+
+    if(bad_lines.includes(clips_processed[currrent_video_id])){
+        index = bad_lines.indexOf(clips_processed[currrent_video_id])
+        bad_lines.splice(index, 1)
+        document.getElementById("badoutput").textContent = bad_lines.join("\n")
+    }
 }
 
 function loadVideo(value) {
+    console.log("Loading Video with id " + value)
     var video = document.getElementsByTagName('video')[0];
     var sources = video.getElementsByTagName('source');
     sources[0].src = '/static/videos/'+value+'.mp4';
@@ -29,31 +73,59 @@ function loadVideo(value) {
 
 $(function() {
     $('#nextbutton').on('click', function(e) {
+        document.getElementById("nextbutton").disabled = true;
+        setTimeout(function(){document.getElementById("nextbutton").disabled = false;},10000);
+
         e.preventDefault()
 
         if (firstRun) {
-            
-            c = "despacito"
-            $.post( "/initial_download", {post_urls: c})
+            var lines = document.getElementById("input-textarea").value.split('\n');
+            if(document.getElementById("input-textarea").value.length > 5){
+                    
+                c = "despacito"
+                $.post( "/initial_download", {
+                    post1:lines[0], 
+                    post2:lines[1],
+                    post3:lines[2]
+                })
 
-            alert("First few videos are being downloaded, video player will update in a few seconds...")
-            setTimeout(() => {  loadVideo(1); }, 10000);
-            firstRun = false
+                clips_processed.push(lines[0])
+                clips_processed.push(lines[1])
+                clips_processed.push(lines[2])
+
+
+                lines.shift();
+                lines.shift();
+                lines.shift();
+
+                var new_lines = lines.join("\n")
+                document.getElementById("input-textarea").value = new_lines
+
+                alert("First few videos are being downloaded, video player will update in a few seconds...")
+                setTimeout(() => {  loadVideo(0); }, 10000);
+                firstRun = false
+
+                $.post( "/reset_folders", {
+                    data:"fuck you get out my code"
+                })
+            } else {
+                alert("Do you have disabilities? Input some clips first you bellend.")
+            }
+
             
         } else {
             loadVideo(currrent_video_id+1)
             currrent_video_id = currrent_video_id+1
             if(document.getElementById("input-textarea").value.length > 5){
-                console.log("Downloading Video " + current_video)
-                $.post( "/download_video", {post_url: current_video})
+                var lines = document.getElementById("input-textarea").value.split('\n');
+                console.log("Downloading " + lines[0])
+                $.post( "/download_video", {post_url: lines[0]})
     
-                console.log(current_video)
-                clips = document.getElementById("input-textarea").value.split("\n");
-                current_video = clips.pop();
-                console.log(current_video)
-            
-                document.getElementById("input-textarea").value = clips.join("\n")
-    
+                clips_processed.push(lines[0])
+                lines.shift()
+                
+                var new_lines = lines.join("\n")
+                document.getElementById("input-textarea").value = new_lines
             } else {
                 alert("No videos left...")
             }
